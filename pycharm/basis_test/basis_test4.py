6a3480df5ad142a81478504c2b75c17cae3a39d2
clary2000 <claryguru2000@gmail.com>
2022-07-05 09:51:40 +0200

    more thesis

--- pycharm/basis_test/basis_test4.py
+++ pycharm/basis_test/basis_test4.py
@@ -2,6 +2,7 @@
 from softioc import softioc, builder, asyncio_dispatcher
 import asyncio
 from vimba import *
+import numpy as np
 
 class Camera:
     def __init__(self):
@@ -20,27 +21,31 @@ class Camera:
         asyncio.run_coroutine_threadsafe(self.epics.run(), dispatcher.loop)
 
 class Epics:
-    def __init__(self, data_a):
-        builder.SetDeviceName("MY-DEVICE-PREFIX")
+    def __init__(self, cam):
+        builder.SetDeviceName("ALMUT")
 
         # Create some records
         self.ai = builder.aIn('AI', initial_value=5)
         self.ao = builder.aOut('AO', initial_value=12.45, always_update=True,
-                          on_update=lambda v: self.ai.set(v))
+                          on_update_name=lambda v, n: self.on_update(n,v))
 
         # Boilerplate get the IOC started
         builder.LoadDatabase()
-        self.data_a = data_a
+        self.data_a = cam.data_a
+
+    def on_update(self, ao_name, value):
+        print(ao_name, " change to ", value)
+
 
     async def run(self):
         while True:
             self.ai.set(self.data_a.params[0])
             print("ai set to", self.data_a.params[0])
-            await asyncio.sleep(0)
+            await asyncio.sleep(1)
 
 class DataAnalyzer:
-    def __init__(self, ia):
-        self.ia = ia
+    def __init__(self, cam):
+        self.ia = cam.ia
         self.params = [0,0]
 
     async def analyze(self):
@@ -50,26 +55,20 @@ class DataAnalyzer:
             print("params berechnet ", self.params)
             await asyncio.sleep(2)
 
-class ImageA:
-    def __init__(self):
-        self.index= 1
+class ImageA():
+    def __init__(self, cam):
+        self.index = 1
+
+    def load_im(self):
+        print("loaded image ", self.index)
+        self.index += 1
 
     async def aquire(self):
-        print("hi")
-        with Vimba.get_instance() as vimba:
-            cams = vimba.get_all_cameras()
-            print(cams)
-            if len(cams) > 0:
-                with cams[0] as cam:
-                    cam.ExposureAuto = 'On'
-                    while True:
-                        frame = cam.get_frame()
-                        self.index += 1
-                        print('Got {} {}'.format(frame, self.index), flush=True)
-                        await asyncio.sleep(0)
-            else:
-                print("no camera detected")
-                await asyncio.sleep(0)
+        while True:
+            self.load_im()
+            await asyncio.sleep(1)
+        print("reached end of folder")
+        await asyncio.sleep(0)
 
 
 
@@ -82,7 +81,7 @@ if __name__ == '__main__':
     softioc.iocInit(dispatcher)
 
     # Start processes required to be run after iocInit
-    cam.run()
+    cam.run(dispatcher)
 
 
     # Finally leave the IOC running with an interactive shell.
